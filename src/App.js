4070b7ef9db97275bf4329fef5812e43eecb8e64
Kristina <kristinakocur2@gmail.com>
2021-11-16 19:31:14 +0100

    Starting again-cleaned up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Search from "./Search";
+import Weather from "./Weather";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <Search />
+      <Weather />
+      <footer>
+        <p>This project was coded by <a href="https://app.netlify.com/sites/pedantic-golick-389ca6" target="_blank">
+        Kristina Kocur </a> and it is open-sourced on <a href="https://github.com/kika1809/react-weather" target="">GitHub</a>.</p>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,31 +3,7 @@ import axios from "axios";
 
 import "./App.css";
 
-export default function Search() {
-  let [city, newCity] = useState("");
-  let [info, newInfo] = useState("");
-  let [tempe, newTempe] = useState(null);
+export default function Weather() {
 
-  function typedinData(event) {
-    newCity(event.target.value);
-  }
-  function submittedAction(event) {
-    event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=92161eb593fedf6f773cc741f318b677&units=metric`;
-    axios.get(apiUrl).then(getTempe);
-  }
-  function getTempe(response) {
-    newTempe(Math.round(response.data.main.temp));
-  }
-
-  return (
-    <div className="WeatherInfo">
-      <form onSubmit={submittedAction}>
-        <input type="search" onChange={typedinData} />
-        <input type="submit" value="Search" />
-      </form>
-
-      <h2>{info}</h2>
-    </div>
-  );
+  return (<p>Hello from Weather</p>);
 }
