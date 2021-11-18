0085baec44a70ecb90b6f3b2292d1079a4859956
Kristina <kristinakocur2@gmail.com>
2021-11-18 19:49:33 +0100

    Yarn fix-try2

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ export default function App() {
         <div className="container">
       <Weather defaultCity="Split"/>
       <footer>
-        <p>This project was coded by <a href="https://app.netlify.com/sites/pedantic-golick-389ca6" target="_blank">
-        Kristina Kocur</a> and it is open-sourced on <a href="https://github.com/kika1809/react-weather" target="_blank">GitHub</a>, hosted on <a href="https://github.com/kika1809/react-weather" target="_blank">Netlify</a>.</p>
+        <p>This project was coded by <a href="https://app.netlify.com/sites/pedantic-golick-389ca6" target="_blank" 
+            rel="noopener noreferrer">
+        Kristina Kocur</a> and it is open-sourced on <a href="https://github.com/kika1809/react-weather" target="_blank" 
+            rel="noopener noreferrer">GitHub</a>, hosted on <a href="https://github.com/kika1809/react-weather" target="_blank" 
+            rel="noopener noreferrer">Netlify</a>.</p>
       </footer>
       </div>
     </div>
